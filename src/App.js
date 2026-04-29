470d1093c3c5ca42b6c8419463a659233d82a249
FadwaYD <yadinefadwa06@gmail.com>
2026-04-29 19:01:21 +0000

    Premier commit React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [nom, setNom] = useState("");
+
+  const envoyer = () => {
+    axios.post("/backend-j2ee/api/ajouter", {
+      nom: nom
+    })
+    .then(res => {
+      console.log(res.data);
+      alert(res.data.message);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Test connexion React → J2EE</h2>
+      <input onChange={(e) => setNom(e.target.value)} />
+      <button onClick={envoyer}>Envoyer</button>
     </div>
   );
 }
